c2579791a8253d5fc93f785fc65d397cc717f042
RioAdistya <rioputra191101@gmail.com>
2022-11-28 10:05:38 +0700

    update

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 const app = require('../app');
-const APP_PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(APP_PORT, () => {
-  console.log(`Server running on port ${APP_PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
